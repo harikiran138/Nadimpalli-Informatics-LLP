1445a04267943c5926776736d41514bddb9c9bb3
harikiran138 <155863015+harikiran138@users.noreply.github.com>
2025-12-05 07:31:25 +0530

    login

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -25,7 +25,7 @@ export function Navbar() {
             </div>
 
             {/* Login Button - Fixed top right */}
-            <div className="fixed top-6 right-6 z-50 hidden md:block">
+            <div className="fixed top-6 right-6 z-50">
                 <Link href="/login">
                     <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 group">
                         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
